10df65eb63a7d47e75b5f8d2b3c3d1f3655c8cee
Martin <marcinw2001@gmail.com>
2023-12-04 10:36:49 +0100

    .

--- Front/system/src/components/Start.jsx
+++ Front/system/src/components/Start.jsx
@@ -9,21 +9,6 @@ export const Start = ({ selAlgo, selFitfun }) => {
     <div id='sectionStart' className='section'>
       <p className='sectionTitle'>Uruchom</p>
       <hr />
-      <form>
-        <label>
-          Populacja:
-          <input type='number' />
-        </label>
-        <label>
-          Iteracje:
-          <input type='number' />
-        </label>
-        <label>
-          Wymiar:
-          <input type='number' />
-        </label>
-        <input type='submit' value={"Start"} />
-      </form>
     </div>
   )
 }
